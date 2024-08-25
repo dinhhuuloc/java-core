68301d9c15f4cc5f488edba8d5efd1d98edea932
dinhhuuloc <dinhhuuloc.work@gmail.com>
2024-08-25 21:47:43 +0700

    s1 hello world

---
+++ src/javacore/HelloWorld.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+
+	}
+}
